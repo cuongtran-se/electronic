38e70e5f20771fe3134b9494c7ffd7e12b228a30
tranquoccuong0179 <tranquoccuong0179@gmail.com>
2025-07-25 17:04:21 +0700

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,43 +1,54 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 export const Home = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Thi công trạm biến áp Bình Thuận - Trung Hưng</title>
-        <meta name="description" content="Công ty Trung Hưng chuyên tư vấn, thiết kế và thi công lắp đặt trạm biến áp, hệ thống điện trung hạ thế, chiếu sáng tại Bình Thuận." />
-        <meta name="keywords" content="thi công trạm biến áp, điện trung thế, máy biến áp, electronic, hệ thống chiếu sáng, điện công nghiệp" />
+        <meta
+          name="description"
+          content="Harmon là dịch vụ thi công trạm biến áp, hệ thống điện trung thế tại Bình Thuận, do công ty Trung Hưng thực hiện với uy tín và chuyên nghiệp."
+        />
+        <meta
+          name="keywords"
+          content="thi công trạm biến áp, điện trung thế, máy biến áp, electronic, hệ thống chiếu sáng, điện công nghiệp, Harmon, Trung Hưng"
+        />
 
         <meta property="og:title" content="Trạm biến áp Bình Thuận - Thi công chuyên nghiệp" />
-        <meta property="og:description" content="Trung Hưng chuyên lắp đặt trạm điện, chiếu sáng công cộng, thi công điện trung thế chất lượng cao." />
-        <meta property="og:image" content="https://landing.hangout.com.vn/image/h5-533x400.jpg" />
-        <meta property="og:url" content="https://landing.hangout.com.vn" />
+        <meta
+          property="og:description"
+          content="Harmon là dịch vụ thi công trạm biến áp, hệ thống điện trung thế tại Bình Thuận. Được thực hiện bởi công ty Trung Hưng chuyên nghiệp, uy tín."
+        />
+        <meta property="og:image" content="https://harmon.love/image/h5-533x400.jpg" />
+        <meta property="og:url" content="https://harmon.love" />
         <meta property="og:type" content="website" />
       </Helmet>
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
-      <div className="container mx-auto max-w-5xl text-gray-500">
-        <div className="py-10">
-          <div className="text-3xl">Dịch vụ thi công xây lắp trạm biến áp tại Bình Thuận</div>
-          <p className="text-base mt-4"><span className="font-bold text-black">Công ty TNHH Xây Lắp Điện Trung Hưng</span> có nhiều năm kinh nghiệm trong lĩnh vực Tư vấn giải pháp, Thi công lắp đặt hệ thống điện, trạm điện, hệ thống chiếu sáng, máy biến áp, và các sản phẩm liên quan trong ngành điện</p>
-          <div className="flex justify-center items-center my-5">
-            <img src="/image/h5-533x400.jpg" alt="Trạm biến áp tại Bình Thuận" className="max-w-full h-auto" />
+
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
+        <div className="container mx-auto max-w-5xl text-gray-500">
+          <div className="py-10">
+            <div className="text-3xl">Dịch vụ thi công xây lắp trạm biến áp tại Bình Thuận</div>
+            <p className="text-base mt-4"><span className="font-bold text-black">Công ty TNHH Xây Lắp Điện Trung Hưng</span> có nhiều năm kinh nghiệm trong lĩnh vực Tư vấn giải pháp, Thi công lắp đặt hệ thống điện, trạm điện, hệ thống chiếu sáng, máy biến áp, và các sản phẩm liên quan trong ngành điện</p>
+            <div className="flex justify-center items-center my-5">
+              <img src="/image/h5-533x400.jpg" alt="Trạm biến áp tại Bình Thuận" className="max-w-full h-auto" />
+            </div>
+            <p className="text-base font-bold text-black">♦ Thiết kế, thi công đường dây trung hạ thế & trạm biến áp đến cấp 35kV.</p>
+            <p className="text-base font-bold text-black">♦ Bảo trì, thí nghiệm các công trình lưới điện & TBA trung hạ thế đến cấp 35kV.</p>
+            <p className="text-base font-bold text-black">♦ Thiết kế, thi công lắp đặt hệ thống chiếu sáng công cộng, nhà xưởng công nghiệp.</p>
+            <div className="flex justify-center items-center my-5">
+              <img src="/image/h1-533x400.jpg" alt="Hệ thống chiếu sáng công cộng" className="max-w-full h-auto" />
+            </div>
+            <p className="text-base font-bold text-black underline">Cam kết của công ty:</p>
+            <p className="text-base font-bold text-black">✓ Cung cấp vật tư, thiết bị chính hãng, có đầy đủ giấy chứng nhận CO, CQ.</p>
+            <p className="text-base font-bold text-black">✓ Thi công trên tinh thần đảm bảo kịp tiến độ cho nhà đầu tư.</p>
+            <p className="text-base font-bold text-black">✓ Kỹ thuật luôn được cải tiến để phù hợp với từng đặc điểm công trình.</p>
+            <p className="text-base font-bold text-black">✓ Luôn hỗ trợ nhiệt tình những khó khăn và thắc mắc của quí khách hàng.</p>
+            <p className="text-base font-bold text-black">✓ Giá luôn cạnh tranh mang lại hiệu quả kinh tế cao nhất cho chủ đầu tư.</p>
+            <p className="text-gray-500 mt-4">Là đơn vị hoạt động trong lĩnh vực tư vấn, thi công đường dây trung & hạ thế. Trạm biến áp & hệ thống chiếu sáng. Trải qua nhiều năm hoạt động cùng sự tư vấn và hổ trợ của đội ngũ kỹ sư, kỹ thuật viên lâu năm có tâm huyết với phương châm <span className="italic font-bold">“ Ngày mai sẽ hơn hôm nay”</span> để phát triển thành một đơn vị tư vấn, thi công chuyên nghiệp. Qua hàng trăm công trình tiêu biểu, những kinh nghiệm đã được đúc kết. <span className="font-bold">Công ty TNHH xây lắp điện Trung Hưng</span> tự hào là đối tác tin cậy của quý khách hàng trong và ngoài tỉnh Bình Thuận.</p>
           </div>
-          <p className="text-base font-bold text-black">♦ Thiết kế, thi công đường dây trung hạ thế & trạm biến áp đến cấp 35kV.</p>
-          <p className="text-base font-bold text-black">♦ Bảo trì, thí nghiệm các công trình lưới điện & TBA trung hạ thế đến cấp 35kV.</p>
-          <p className="text-base font-bold text-black">♦ Thiết kế, thi công lắp đặt hệ thống chiếu sáng công cộng, nhà xưởng công nghiệp.</p>
-          <div className="flex justify-center items-center my-5">
-            <img src="/image/h1-533x400.jpg" alt="Hệ thống chiếu sáng công cộng" className="max-w-full h-auto" />
-          </div>
-          <p className="text-base font-bold text-black underline">Cam kết của công ty:</p>
-          <p className="text-base font-bold text-black">✓ Cung cấp vật tư, thiết bị chính hãng, có đầy đủ giấy chứng nhận CO, CQ.</p>
-          <p className="text-base font-bold text-black">✓ Thi công trên tinh thần đảm bảo kịp tiến độ cho nhà đầu tư.</p>
-          <p className="text-base font-bold text-black">✓ Kỹ thuật luôn được cải tiến để phù hợp với từng đặc điểm công trình.</p>
-          <p className="text-base font-bold text-black">✓ Luôn hỗ trợ nhiệt tình những khó khăn và thắc mắc của quí khách hàng.</p>
-          <p className="text-base font-bold text-black">✓ Giá luôn cạnh tranh mang lại hiệu quả kinh tế cao nhất cho chủ đầu tư.</p>
-          <p className="text-gray-500 mt-4">Là đơn vị hoạt động trong lĩnh vực tư vấn, thi công đường dây trung & hạ thế. Trạm biến áp & hệ thống chiếu sáng. Trải qua nhiều năm hoạt động cùng sự tư vấn và hổ trợ của đội ngũ kỹ sư, kỹ thuật viên lâu năm có tâm huyết với phương châm <span className="italic font-bold">“ Ngày mai sẽ hơn hôm nay”</span> để phát triển thành một đơn vị tư vấn, thi công chuyên nghiệp. Qua hàng trăm công trình tiêu biểu, những kinh nghiệm đã được đúc kết. <span className="font-bold">Công ty TNHH xây lắp điện Trung Hưng</span> tự hào là đối tác tin cậy của quý khách hàng trong và ngoài tỉnh Bình Thuận.</p>
         </div>
       </div>
-    </div>
     </>
   )
 }
